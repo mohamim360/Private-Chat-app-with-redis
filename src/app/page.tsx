c0c9d0ee392b0108a9b434013d5e73db1ecc5035
mohamim360 <mohamim360@gmail.com>
2025-12-11 18:04:17 +0600

    feat: add room creation functionality and integrate with redis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,27 @@
 "use client"
 
+import { client } from "@/lib/client";
+import { useMutation } from "@tanstack/react-query";
 import { nanoid } from "nanoid";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const ANIMALS = ["wolf", "cat", "dragon", "lion", "snake", "panda"]
 const STORAGE_KEY = "chat_username"
 
 const generateUsername = () => {
-  const word = ANIMALS[Math.floor(Math.random() * ANIMALS.length )]
+  const word = ANIMALS[Math.floor(Math.random() * ANIMALS.length)]
   return `anonymous-${word}-${nanoid(5)}`
 }
 export default function Home() {
   const [username, setUsername] = useState("")
+  const router = useRouter()
 
   useEffect(() => {
     const main = () => {
       const stored = localStorage.getItem(STORAGE_KEY)
 
-      if(stored){
+      if (stored) {
         setUsername(stored)
         return
       }
@@ -29,6 +33,17 @@ export default function Home() {
 
     main()
   }, [])
+
+  const { mutate: createRoom } = useMutation({
+    mutationFn: async () => {
+      const res = await client.room.create.post()
+
+      if(res.status == 200){
+        router.push(`/room/${res.data?.roomId}`)
+      }
+    }
+  })
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4">
       <div className="w-full max-w-md space-y-8">
@@ -53,7 +68,9 @@ export default function Home() {
               </div>
             </div>
 
-            <button className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50">
+            <button
+              onClick={() => createRoom()}
+              className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50">
               CREATE SECURE ROOM
             </button>
 
