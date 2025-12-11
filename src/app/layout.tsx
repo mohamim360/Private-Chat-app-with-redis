cbb7d93d4f89054a2139a2d13eb136e0457926ec
mohamim360 <mohamim360@gmail.com>
2025-12-11 17:35:51 +0600

    feat: set up elysiajs and tanstack query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
-import {JetBrains_Mono } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "@/components/provider";
 
 const jetBrainMono = JetBrains_Mono({
   variable: "--font-jetbrains-mono",
@@ -23,7 +24,9 @@ export default function RootLayout({
       <body
         className={`${jetBrainMono.variable} antialiased`}
       >
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
